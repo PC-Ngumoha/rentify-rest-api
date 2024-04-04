1de4c32f36d7cef27920ffacefd8879e2384fc7b
PC-Ngumoha <pcngumoha@gmail.com>
2024-04-04 14:46:06 +0100

    Update: register user in admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,2 +1,8 @@
-# TODO: Create custom admin
-from django.contrib import admin  # noqa
+"""
+Registering the project DB models in the app admin.
+"""
+from django.contrib import admin
+
+from core import models
+
+admin.site.register(models.User)
